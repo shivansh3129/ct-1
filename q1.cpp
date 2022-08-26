29659b678d46c964979b4e2b4e1e840463887810
shivansh3129 <78163595+shivansh3129@users.noreply.github.com>
2022-08-26 15:37:21 +0530

    Add files via upload

---
+++ q1.cpp
@@ -0,0 +1,55 @@
+/* Name - Shivansh Dhingra
+roll no.-2010990669
+Set no - Test 05
+Question no. One*/
+
+#include <stdio.h>
+ 
+
+void findMaxLenSubarray(int nums[], int n, int S)
+{
+    
+    int len = 0;
+ 
+    
+    int ending_index = -1;
+ 
+    
+    for (int i = 0; i < n; i++)
+    {
+        int target = 0;
+ 
+        
+        for (int j = i; j < n; j++)
+        {
+            
+            target += nums[j];
+ 
+            
+            if (target == S)
+            {
+                
+                if (len < j - i + 1)
+                {
+                    len = j - i + 1;
+                    ending_index = j;
+                }
+            }
+        }
+    }
+ 
+
+    printf("[%d, %d]", ending_index - len + 1, ending_index);
+}
+ 
+int main(void)
+{
+    int nums[] = { 5, 6, -5, 5, 3, 5, 3, -2, 0 };
+    int target = 8;
+ 
+    int n = sizeof(nums)/sizeof(nums[0]);
+ 
+    findMaxLenSubarray(nums, n, target);
+ 
+    return 0;
+}
---
+++ q2.cpp
@@ -0,0 +1,39 @@
+/* Name -Shivansh Dhingra
+Roll no.-2010990669
+Set no.- Test 05
+Question no. two*/
+
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+ 
+
+void printAllSubarrays(int nums[], int n)
+{
+    
+    for (int i = 0; i < n; i++)
+    {
+        int sum = 0;
+ 
+        
+        for (int j = i; j < n; j++)
+        {
+            
+            sum += nums[j];
+ 
+            if (sum == 0) {
+                cout << "Subarray [" << i << "…" << j << "]\n";
+            }
+        }
+    }
+}
+ 
+int main()
+{
+    int nums[] = { 3, 4, -7, 3, 1, 3, 1, -4, -2, -2 };
+    int n = sizeof(nums)/sizeof(nums[0]);
+ 
+    printAllSubarrays(nums, n);
+ 
+    return 0;
+}
